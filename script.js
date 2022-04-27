c54fb5e2734b9199f2987c68f374fdb3e86f9d7f
Joseph Yika <josephyika123@gmail.com>
2022-04-27 16:55:20 +0200

    Added Login and Register Pages

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navbarElements = document.getElementsByClassName('navbar-elements')[0]
 toggleButton.addEventListener('click', () => {
     navbarElements.classList.toggle('active')
 })
+
+// Login and Register Switch
+var l = document.getElementById("login");
+var r = document.getElementById("register");
+var b = document.getElementById("btn");
+
+function register(){
+    l.style.left = "-400px";
+    r.style.left = "50px";
+    b.style.left = "50px";
+}
